c4700b0c57e81888544abba26095f36cb4262aae
unknown <oskaz.zadora@gmail.com>
2020-01-20 15:02:53 +0100

    I dont understand this method

--- Zoo.cs
+++ Zoo.cs
@@ -6,5 +6,9 @@ namespace TestProjectForCleanCodeSpontan
     public class Zoo
     {
         List<Animal> Animals{get;set;}
+
+        public void VeryLongMethodOfNewParametersAndVeryVeryUnpredictableZachoeaniw(){
+            int x=0;
+        }
     }
 }
