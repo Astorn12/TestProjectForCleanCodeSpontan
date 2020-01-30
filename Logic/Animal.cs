37f8cfc4749c823fa0b87ee19bf7865900107bfe
unknown <oskaz.zadora@gmail.com>
2020-01-30 07:08:17 +0100

    MSFDF

--- Logic/Animal.cs
+++ Logic/Animal.cs
@@ -7,8 +7,44 @@ namespace TestProjectForCleanCodeSpontan.Logic
         public int NibyNowaNibyStara;
         public List<Tooth> Teeth { get; set; }
         public string VeryLongNameOfVariableToCheckCleanCodeSpontanIfItWorking{get;set;}
-        public void RunIfSomeoneIsChasingYou(){
+        public void RunIfSomeoneIsChasingYou(int x, int t, int h, int j, int l){
 
+        }
+
+        public void VeryLongMethod(){
+            int t=5;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
+            t++;
         }
     }
 }
